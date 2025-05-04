ef6e71ea236cc6942e84c9b6e9adc04c78ebf325
Pitrouflette <charlesdubesset@gmail.com>
2025-05-04 15:57:44 +0200

    traduction

--- v3/public/js/protected-page.js
+++ v3/public/js/protected-page.js
@@ -6,7 +6,6 @@ if (localStorage.getItem('isLoggedIn') != "true") {
   window.location.href = 'login.html';
 } else {
   loadHeader();
-  //loadFooter();
 }
 
 sockets.on("sendData", (data) => {
@@ -17,10 +16,10 @@ sockets.on("sendData", (data) => {
   console.log(data.admin);
   if (data.admin == "true") {
     nav.innerHTML = `
-      <a href="post.html" class="nav-link">accueil</a>
+      <a href="post.html" class="nav-link">home</a>
       <a href="chat.html" class="nav-link">chat</a>
-      <a href="poster.html" class="nav-link">poster</a>
-      <a href="profil.html" class="nav-link">compte</a>
+      <a href="poster.html" class="nav-link">tell</a>
+      <a href="profil.html" class="nav-link">account</a>
       <a href="admin.html" class="nav-link admin">admin panel</a>
     `;
   }
@@ -47,19 +46,5 @@ async function loadHeader() {
     }
   } catch (error) {
     console.error('Erreur lors du chargement du header:', error);
-  }
-}
-
-async function loadFooter() {
-  try {
-    const response = await fetch('../html/footer.html');
-    if (response.ok) {
-      const footerContent = await response.text();
-      document.getElementById('footer-container').innerHTML = footerContent;
-    } else {
-      console.error('Erreur lors du chargement du footer:', response.statusText);
-    }
-  } catch (error) {
-    console.error('Erreur lors du chargement du footer:', error);
   }
 }
